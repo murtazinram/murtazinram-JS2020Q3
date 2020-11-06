3cb46f24ed09bbb8fd31793ce666afde9c851562
murtazinram <murtazinram@gmail.com>
2020-11-06 15:27:57 +0300

    feat: add class GemPuzzle, add method getBoard with function mixedArray

---
+++ gem-puzzle/getBoard.js
@@ -0,0 +1,45 @@
+export function getBoard(size, board) {
+    board.innerHTML = '';
+    let count = 0;
+    let arr = []
+
+    for (let i = 0; i < 16; i++) {
+        arr.push(i);
+    }
+    console.log(arr)
+    mixedArray(arr)
+    console.log(arr)
+    for (let i = 1; i <= size; i++) {
+        for (let j = 1; j <= size; j++) {
+            const cell = document.createElement('div');
+            cell.id = `cell-${i}-${j}`;
+            cell.classList.add('cell');
+
+            if (count !== Math.pow(size, 2)) {
+                if (arr[count] !== 0) {
+                    cell.classList.add('light')
+                }
+                cell.classList.add('number');
+                cell.innerHTML = arr[count]
+            }
+            if (arr[count] === 0) {
+                cell.innerHTML = ''
+                cell.classList.add('empty');
+            }
+            count++
+            board.appendChild(cell);
+        }
+    }
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
+
+function mixedArray(arr) {
+    let randIndex;
+    for (let i in arr) {
+        let x = arr[i];
+        randIndex = Math.floor(Math.random() * arr.length);
+        arr[i] = arr[randIndex];
+        arr[randIndex] = x;
+    }
+    return arr
+}
--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -1,1 +1,43 @@
-// todo
+import { getBoard } from './getBoard.js'
+// import getButtons from './getButtons'
+class GemPuzzle {
+    constructor() {
+        this._moves = 0;
+        this._time = 0;
+        this._size = 4;
+        this._mixed = false;
+        this._save = localStorage.getItem('save');
+    }
+
+
+    getBoard () {
+        return getBoard(this._size, this.board)
+    }
+
+    getButtons(){
+        return getButtons()
+    }
+
+    init(){
+        document.body.innerHTML = ''
+
+        this.wrapper = document.createElement('main');
+        this.board = document.createElement('div');
+
+        this.wrapper.classList.add('wrapper');
+        this.board.classList.add('board');
+
+        this.wrapper.appendChild(this.board);
+
+        document.body.appendChild(this.wrapper);
+
+    }
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const gemPuzzle = new GemPuzzle();
+
+    gemPuzzle.init();
+    gemPuzzle.getBoard()
+});
