b5a9131055559e9a48f7e71c5c7b726a25b3683c
murtazinram <murtazinram@gmail.com>
2020-11-07 00:11:34 +0300

    refactor: script.js

--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -1,24 +1,43 @@
 import {getBoard} from './getBoard.js'
 import {checkResult} from './checkResult.js'
 import {moveCell} from './moveCell.js'
+import {getHeader} from './getHeader.js'
+import {getTime} from './getTime.js'
 
 // import getButtons from './getButtons'
 class GemPuzzle {
     constructor() {
         this._moves = 0;
-        this._time = 0;
+        this._startTime = 0;
+        this._time = null;
         this._size = 4;
         this._mixed = false;
         this._save = localStorage.getItem('save');
     }
 
-    getHeader(){
-
+    getHeader() {
+        getHeader(this.header)
     }
-
 
     getBoard() {
         return getBoard(this._size, this.board)
+    }
+
+    getTime(startTime = this._startTime) {
+        getTime(startTime)
+    }
+
+    startDuration() {
+        this._time = setInterval(this.getTime, 1000, this._startTime);
+    }
+
+    clearDuration() {
+        clearInterval(this._time);
+    }
+
+    getMoves() {
+        const moves = document.querySelector('#moves');
+        moves.textContent = this._moves.toString().padStart(3, '0');
     }
 
     checkResult() {
@@ -36,6 +55,10 @@ class GemPuzzle {
         this.board.addEventListener('click', (e) => {
             moveCell(e.target)
             this.checkResult()
+            this._moves++
+            this.getMoves()
+            this._startTime = new Date
+            if (this._moves === 1) this.startDuration()
         });
     }
 
@@ -44,14 +67,16 @@ class GemPuzzle {
 
         this.wrapper = document.createElement('main');
         this.board = document.createElement('div');
+        this.header = document.createElement('div');
 
         this.wrapper.classList.add('wrapper');
         this.board.classList.add('board');
+        this.header.classList.add('header');
 
+        this.wrapper.appendChild(this.header);
         this.wrapper.appendChild(this.board);
 
         document.body.appendChild(this.wrapper);
-
     }
 }
 
@@ -61,6 +86,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     gemPuzzle.init();
     gemPuzzle.getBoard()
+    gemPuzzle.getHeader()
     gemPuzzle.addListeners()
-
 });
